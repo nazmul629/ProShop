60394a55e0e5511111707b3418ec8d58e1e84e49
namzul629 <nazmulhossain.qnh@gmail.com>
2021-02-24 00:28:51 +0600

    Install ract-bootstarp and header and footer set in the workspace

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { Container} from 'react-bootstrap'
+import Header from './component/Header'
+import Footer from './component/Footer'
 
 function App() {
   return (
+    
     <div className="App">
-          <h1> Welcome to ProShop</h1>
+     
+          <Header />
+
+          <main className="my-3">
+            <Container>
+              <h1> Welcome to ProShop</h1>
+            </Container>
+          </main>
+          <Footer />
     </div>
   );
 }
