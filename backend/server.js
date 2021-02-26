9a493b1b5bb78f20445b7c5b65d544b8ccc0fe91
namzul629 <nazmulhossain.qnh@gmail.com>
2021-02-26 21:24:25 +0600

    fatching product form express route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,9 @@
-// const express = require('express')
-// const products = require('./data/products')
-// const dotenv = require('dotenv')
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDB from './config/db.js' 
 import colors from 'colors'
-
+// import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 
 const app = express()
@@ -17,17 +14,8 @@ connectDB()
 app.get('/', (req,res) =>{
     res.send("API is running... ")
 })
+app.use ('/api/products',productRoutes)
 
-
-app.get('/api/products', (req,res) =>{
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req,res) =>{
-
-    const product = products.find(p =>p._id === req.params.id)
-    res.json(product)
-})
 
 app.set('port', process.env.PORT || 3000);
 
