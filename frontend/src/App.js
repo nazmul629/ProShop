6123e8e2d5837cc6387ea10ab623442e079818cc
namzul629 <nazmulhossain.qnh@gmail.com>
2021-02-27 21:22:08 +0600

    Added CardScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './component/Header'
 import Footer from './component/Footer'
 import HomeScreen from './screen/HomeScreen'
 import ProductScreen from './screen/ProductScreen'
+import CartScreen from './screen/CartScreen'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Container> 
               <Route path="/" component = { HomeScreen } exact   />
                <Route path="/product/:id" component= {ProductScreen} />
+               <Route path="/cart/:id?" component= {CartScreen} />
 
             </Container>
           </main>
