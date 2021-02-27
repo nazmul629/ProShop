12f9bb824f10615d6afa4d2eea6b333f74f877d1
namzul629 <nazmulhossain.qnh@gmail.com>
2021-02-27 16:13:27 +0600

    Redux state into homepage screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,18 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools} from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productReducers'
 
+const reducer = combineReducers({
+    productList:productListReducer
+})
 
-const reducer = combineReducers({})
 const initialState = {}
 const middleware = [thunk]
 const store = createStore(
     reducer,
     initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    // composeWithDevTools(applyMiddleware(...middleware))
+   composeWithDevTools(applyMiddleware(...middleware))
     )
 
 export default store
